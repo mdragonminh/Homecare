c926318a8107168cf859e4583ae6858b7ce95a7a
QuanHQ <quanqhhq@gmail.com>
2025-09-17 15:59:36 +0700

    add registerDto in service

---
+++ HomeServicePlatform/HSP.Service/Dtos/AuthenticationDto/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿namespace HSP.Service.Dtos.AuthenticationDto
+{
+	public class RegisterRequestDto
+	{
+		public string Email { get; set; } = string.Empty;
+		public string FullName { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+
+	public class RegisterResponseDto
+	{
+		public Guid UserId { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string EmailConfirmToken { get; set; } = string.Empty;
+	}
+}
