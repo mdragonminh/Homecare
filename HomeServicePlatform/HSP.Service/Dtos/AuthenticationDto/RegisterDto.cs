24949c502739a446318344032b7fcf01094635c1
QuanHQ <quanqhhq@gmail.com>
2025-09-17 22:51:48 +0700

    add data annotation to handle normal error

--- HomeServicePlatform/HSP.Service/Dtos/AuthenticationDto/RegisterDto.cs
+++ HomeServicePlatform/HSP.Service/Dtos/AuthenticationDto/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace HSP.Service.Dtos.AuthenticationDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HSP.Service.Dtos.AuthenticationDto
 {
 	public class RegisterRequestDto
 	{
+		[Required(ErrorMessage = "Email là bắt buộc.")]
+		[EmailAddress(ErrorMessage = "Định dạng email không hợp lệ.")]
 		public string Email { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Họ và tên là bắt buộc.")]
+		[StringLength(100, ErrorMessage = "Họ và tên không được dài hơn 100 ký tự.")]
 		public string FullName { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Mật khẩu là bắt buộc.")]
+		[StringLength(100, MinimumLength = 8, ErrorMessage = "Mật khẩu phải có độ dài từ 8 đến 100 ký tự.")]
 		public string Password { get; set; } = string.Empty;
 	}
 
