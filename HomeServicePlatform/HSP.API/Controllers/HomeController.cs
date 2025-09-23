74bd307597cc307a086dc44d2124bfebd26b4939
QuanHQ <quanqhhq@gmail.com>
2025-09-24 00:17:52 +0700

    add home api

---
+++ HomeServicePlatform/HSP.API/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using HSP.Service.Dtos.HomeDto;
+using HSP.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSP.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly IHomeService _homeService;
+		public HomeController(IHomeService homeService)
+		{
+			_homeService = homeService;
+		}
+		[HttpPost("create-home")]
+		public async Task<IActionResult> CreateHome([FromBody] CreateHomeDto input)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+			try
+			{
+				var homeId = await _homeService.CreateHomeAsync(input);
+				return Ok(new { HomeId = homeId });
+			}
+			catch (ArgumentException ex)
+			{
+				return BadRequest(new { message = ex.Message });
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(new { message = ex.Message });
+			}
+		}
+	}
+}
