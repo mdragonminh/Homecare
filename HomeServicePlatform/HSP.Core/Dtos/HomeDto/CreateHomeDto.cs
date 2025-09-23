a26c82a72a04561da4d9d7adc840c4a34142dcaa
QuanHQ <quanqhhq@gmail.com>
2025-09-24 00:15:53 +0700

    update move dto from service to core

---
+++ HomeServicePlatform/HSP.Core/Dtos/HomeDto/CreateHomeDto.cs
@@ -0,0 +1,21 @@
+﻿using HSP.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace HSP.Service.Dtos.HomeDto
+{
+	public class CreateHomeDto
+	{
+		[StringLength(100)]
+		public string Name { get; set; } = string.Empty;
+
+		[StringLength(200)]
+		public string Address { get; set; } = string.Empty;
+
+		[Range(-90, 90)]
+		public double Latitude { get; set; }
+		[Range(-180, 180)]
+		public double Longitude { get; set; }
+
+		public Guid CustomerProfileId { get; set; }
+	}
+}
---
+++ HomeServicePlatform/HSP.Core/Dtos/MapDto/CoordinatesDto.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HSP.Core.Dtos.MapDto
+{
+	public class CoordinatesDto
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+	}
+	public class GeocodingResponseDto
+	{
+		[JsonPropertyName("results")]
+		public List<GeocodingResultDto> Results { get; set; }
+		[JsonPropertyName("status")]
+		public string Status { get; set; }
+	}
+	public class GeocodingResultDto
+	{
+		[JsonPropertyName("geometry")]
+		public GeometryDto Geometry { get; set; }
+	}
+	public class GeometryDto
+	{
+		[JsonPropertyName("location")]
+		public LocationDto Location { get; set; }
+	}
+	public class LocationDto
+	{
+		[JsonPropertyName("lat")]
+		public double Lat { get; set; }
+		[JsonPropertyName("lng")]
+		public double Lng { get; set; }
+	}
+}
