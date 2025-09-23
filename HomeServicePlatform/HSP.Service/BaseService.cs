dfd32380bfe5170ade58488d4523e20a9535c5d2
QuanHQ <quanqhhq@gmail.com>
2025-09-23 20:46:27 +0700

    add base service to have shared logic in service

---
+++ HomeServicePlatform/HSP.Service/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using HSP.Core.Interfaces;
+
+namespace HSP.Service
+{
+	public abstract class BaseService
+	{
+		protected readonly IUnitOfWork _unitOfWork;
+
+		protected BaseService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+	}
+}
