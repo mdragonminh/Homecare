d36537ef7d90a47d40ca185761be8023376a174f
QuanHQ <quanqhhq@gmail.com>
2025-09-24 21:16:21 +0700

    configuration pagination

---
+++ HomeServicePlatform/HSP.Core/Dtos/Shared/PagedList.cs
@@ -0,0 +1,22 @@
+﻿namespace HSP.Core.Dtos.Shared
+{
+	public class PagedList<T>
+	{
+		public int CurrentPage { get; }
+		public int TotalPages { get; }
+		public int PageSize { get; }
+		public int TotalCount { get; }
+		public bool HasPrevious => CurrentPage > 1;
+		public bool HasNext => CurrentPage < TotalPages;
+		public List<T> Items { get; }
+
+		public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+		{
+			TotalCount = count;
+			PageSize = pageSize;
+			CurrentPage = pageNumber;
+			TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+			Items = items;
+		}
+	}
+}
---
+++ HomeServicePlatform/HSP.DAL/Extensions/QueryableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using HSP.Core.Dtos.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+
+namespace HSP.DAL.Extensions
+{
+	public static class QueryableExtensions
+	{
+		public static async Task<PagedList<T>> ToPagedListAsync<T>(
+						this IQueryable<T> source,
+						PaginationParams paginationParams) where T : class
+		{
+			var query = source.AsNoTracking();
+			var count = await source.CountAsync();
+
+			if (!string.IsNullOrWhiteSpace(paginationParams.OrderBy))
+			{
+				query = query.OrderBy(paginationParams.OrderBy);
+			}
+			var items = await query
+					.Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
+					.Take(paginationParams.PageSize)
+					.ToListAsync();
+			return new PagedList<T>(items, count, paginationParams.PageNumber, paginationParams.PageSize);
+		}
+		public static IQueryable<T> WhereIf<T>(
+				this IQueryable<T> query,
+				bool condition,
+				Expression<Func<T, bool>> predicate) where T : class
+		{
+			return condition ? query.Where(predicate) : query;
+		}
+	}
+}
