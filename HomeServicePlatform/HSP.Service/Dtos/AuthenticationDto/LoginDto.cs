c3535bce39dba0e5819ad25f7329950b4de2483e
QuanHQ <quanqhhq@gmail.com>
2025-09-29 22:45:55 +0700

    Add DTO for adding a password

--- HomeServicePlatform/HSP.Service/Dtos/AuthenticationDto/LoginDto.cs
+++ HomeServicePlatform/HSP.Service/Dtos/AuthenticationDto/LoginDto.cs
@@ -16,4 +16,14 @@ namespace HSP.Service.Dtos.AuthenticationDto
 		[StringLength(100, MinimumLength = 8, ErrorMessage = "Mật khẩu phải có độ dài từ 8 đến 100 ký tự.")]
 		public string Password { get; set; } = string.Empty;
 	}
+	public class AddPasswordDto
+	{
+		[Required]
+		[DataType(DataType.Password)]
+		public string NewPassword { get; set; }
+
+		[DataType(DataType.Password)]
+		[Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+	}
 }
