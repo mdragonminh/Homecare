75782669c963fdaf9ef6ed94e7920296afa57f84
QuanHQ <quanqhhq@gmail.com>
2025-09-29 22:46:09 +0700

    Implement service logic to add a password

--- HomeServicePlatform/HSP.Service/Interfaces/IAuthenticationService.cs
+++ HomeServicePlatform/HSP.Service/Interfaces/IAuthenticationService.cs
@@ -9,5 +9,6 @@ namespace HSP.Service.Interfaces
 		Task<bool> ConfirmEmail(Guid userId, string token);
 		Task<LoginResponseDto> Login(LoginRequestDto input);
 		Task<LoginResponseDto> GoogleLogin();
+		Task<bool> AddPasswordAsync(Guid userId, AddPasswordDto input);
 	}
 }
