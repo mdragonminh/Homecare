095f7bfb29d02aa0f9cd2b8bf86399c2df2e9676
QuanHQ <quanqhhq@gmail.com>
2025-10-01 18:30:36 +0700

    update reset password dto

--- HomeServicePlatform/HSP.Core/Dtos/AuthenticationDto/ForgetPasswordDto.cs
+++ HomeServicePlatform/HSP.Core/Dtos/AuthenticationDto/ForgetPasswordDto.cs
@@ -12,7 +12,7 @@ namespace HSP.Core.Dtos.AuthenticationDto
 	public class ResetPasswordDto
 	{
 		[Required]
-		public string UserId { get; set; }
+		public Guid UserId { get; set; }
 
 		[Required]
 		public string Token { get; set; }
