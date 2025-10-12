05a55c96bb380014fa3a4d4c595b9cb0bf5616ca
QuanHQ <quanqhhq@gmail.com>
2025-10-12 21:41:30 +0700

    Add ServiceRequestController with nearby technicians endpoint

---
+++ HomeServicePlatform/src/HSP.API/Controllers/ServiceRequestController.cs
@@ -0,0 +1,24 @@
+﻿using HSP.Core.Dtos.ServiceRequestDto;
+using HSP.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSP.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ServiceRequestController : ControllerBase
+	{
+		private readonly IServiceRequestService _serviceRequestService;
+
+		public ServiceRequestController(IServiceRequestService serviceRequestService)
+		{
+			_serviceRequestService = serviceRequestService;
+		}
+		[HttpGet("nearby-technicians")]
+		public async Task<IActionResult> GetNearbyTechnicians([FromQuery] SearchTechnicianInput input)
+		{
+			var result = await _serviceRequestService.SearchNearbyTechniciansAsync(input);
+			return Ok(result);
+		}
+	}
+}
