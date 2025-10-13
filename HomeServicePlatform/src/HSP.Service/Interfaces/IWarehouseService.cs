ef8b515eb117083c7acae4db0546fb5c62cf762a
Doan Hung <hungdoanmanh99@gmail.com>
2025-10-13 18:31:49 +0700

    create warehouse service

---
+++ HomeServicePlatform/src/HSP.Service/Interfaces/IWarehouseService.cs
@@ -0,0 +1,17 @@
+using HSP.Core.Dtos.WarehouseDto;
+using HSP.Core.Dtos.Shared;
+
+namespace HSP.Service.Interfaces
+{
+    public interface IWarehouseService
+    {
+        Task<PagedList<WarehouseListDto>> GetWarehousesAsync(int page = 1, int pageSize = 10, string? searchTerm = null);
+        Task<WarehouseDetailDto?> GetWarehouseByIdAsync(Guid id);
+        Task<WarehouseDto> CreateWarehouseAsync(CreateWarehouseDto input, Guid createdBy);
+        Task<WarehouseDto> UpdateWarehouseAsync(Guid id, UpdateWarehouseDto input, Guid modifiedBy);
+        Task<bool> DeleteWarehouseAsync(Guid id);
+        Task<List<WarehouseListDto>> GetAllWarehousesAsync();
+        Task<bool> WarehouseExistsAsync(Guid id);
+        Task<bool> IsWarehouseNameUniqueAsync(string name, Guid? excludeId = null);
+    }
+}
