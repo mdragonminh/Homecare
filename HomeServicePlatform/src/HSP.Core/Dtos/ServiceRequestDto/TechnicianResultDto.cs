8b8499a9a48fa481f809f341409aef460b1cdffb
QuanHQ <quanqhhq@gmail.com>
2025-10-13 15:24:11 +0700

    Refactor technician search to use service IDs and remove price filters

--- HomeServicePlatform/src/HSP.Core/Dtos/ServiceRequestDto/TechnicianResultDto.cs
+++ HomeServicePlatform/src/HSP.Core/Dtos/ServiceRequestDto/TechnicianResultDto.cs
@@ -5,10 +5,11 @@ namespace HSP.Core.Dtos.ServiceRequestDto
 	public class TechnicianResultDto : BaseEntity<Guid>
 	{
 		public string Name { get; set; }
-		public decimal MinPrice { get; set; }
-		public decimal MaxPrice { get; set; }
+		//public decimal MinPrice { get; set; }
+		//public decimal MaxPrice { get; set; }
 		public double DistanceKm { get; set; }
 		public double Latitude { get; set; }
 		public double Longitude { get; set; }
+		public List<string>? Services { get; set; }
 	}
 }
