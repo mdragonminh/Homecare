2dfeb3ff67d79e6377503f7a9b67b26057881cac
Cao Quang Minh <mdragonminh@gmail.com>
2025-10-14 11:37:42 +0700

    update layout for equip manager

---
+++ HomeServicePlatform/src/frontend/src/pages/equipmentmanager/EquipmentManagerEquipmentPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import EquipmentPage from "../admin/EquipmentPage";
+
+export default function EquipmentManagerEquipmentPage() {
+  return <EquipmentPage />;
+}
---
+++ HomeServicePlatform/src/frontend/src/pages/equipmentmanager/EquipmentManagerWarehousePage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import WarehousePage from "../admin/WarehousePage";
+
+export default function EquipmentManagerWarehousePage() {
+  return <WarehousePage />;
+}
