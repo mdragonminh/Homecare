27dddc2b3a1e1c4303570e9862be1cb80a2b4f48
Cao Quang Minh <mdragonminh@gmail.com>
2025-10-14 11:02:12 +0700

    add route

--- HomeServicePlatform/src/frontend/src/routes/AppRoutes.jsx
+++ HomeServicePlatform/src/frontend/src/routes/AppRoutes.jsx
@@ -5,10 +5,13 @@ import ClientRoutes from "./ClientRoutes";
 import TechnicianRegister from "../pages/Auth/TechnicianRegister";
 import { GoogleCallbackPage } from "../pages/Auth/GoogleCallbackPage";
 import AdminLayout from "../pages/admin/AdminLayout";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import AdminSettingsPage from "../pages/admin/AdminSettingsPage";
 import AccountsPage from "../pages/admin/AccountsPage";
-import TechniciansPage from "../pages/admin/TechniciansPage";
-import WarehousePage from "../pages/admin/WarehousePage";
-import EquipmentPage from "../pages/admin/EquipmentPage";
+import OperatorLayout from "../pages/operator/OperatorLayout";
+import OperatorCustomersPage from "../pages/operator/OperatorCustomersPage";
+import OperatorTechniciansPage from "../pages/operator/OperatorTechniciansPage";
+import OperatorSettingsPage from "../pages/operator/OperatorSettingsPage";
 import Layout from "../layouts/Layout";
 import { AddPasswordPage } from "../pages/Auth/AddPasswordPage";
 import ResetPasswordPage from "../pages/Auth/ResetPasswordPage";
@@ -145,10 +148,21 @@ export default function AppRoutes({
         path="/admin"
         element={<AdminLayout loggedInUser={loggedInUser} />}
       >
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
         <Route path="accounts" element={<AccountsPage />} />
-        <Route path="technicians" element={<TechniciansPage />} />
-        <Route path="warehouses" element={<WarehousePage />} />
-        <Route path="equipments" element={<EquipmentPage />} />
+        <Route path="settings" element={<AdminSettingsPage />} />
+      </Route>
+
+      {/* ---- Operator ---- */}
+      <Route
+        path="/operator"
+        element={<OperatorLayout loggedInUser={loggedInUser} />}
+      >
+        <Route index element={<Navigate to="customers" replace />} />
+        <Route path="customers" element={<OperatorCustomersPage />} />
+        <Route path="technicians" element={<OperatorTechniciansPage />} />
+        <Route path="settings" element={<OperatorSettingsPage />} />
       </Route>
     </Routes>
   );
