34efa6fadad7b5f6cb15758d39b9a43f27556978
QuanHQ <quanqhhq@gmail.com>
2025-10-14 20:22:01 +0700

    Add UpdateLocationAsync to technician profile service

--- HomeServicePlatform/src/HSP.Service/Interfaces/ITechnicianProfileService.cs
+++ HomeServicePlatform/src/HSP.Service/Interfaces/ITechnicianProfileService.cs
@@ -3,13 +3,17 @@ using HSP.Core.Dtos.TechnicianProfileDto;
 
 namespace HSP.Service.Interfaces
 {
-    public interface ITechnicianProfileService
-    {
-        Task<PagedList<TechnicianProfileResponseDto>> GetTechniciansAsync(TechnicianProfileFilterParams filterParams);
-        Task<TechnicianProfileResponseDto?> GetTechnicianByIdAsync(Guid id);
-        Task<bool> ApproveTechnicianAsync(Guid technicianProfileId, string approvedBy);
-        Task<bool> RejectTechnicianAsync(Guid technicianProfileId, string rejectedBy);
-        Task<bool> ApproveTechnicianWithNotificationAsync(Guid technicianProfileId, string approvedBy);
-        Task<bool> RejectTechnicianWithNotificationAsync(Guid technicianProfileId, string rejectedBy);
-    }
+	public interface ITechnicianProfileService
+	{
+		Task<PagedList<TechnicianProfileResponseDto>> GetTechniciansAsync(TechnicianProfileFilterParams filterParams);
+		Task<TechnicianProfileResponseDto?> GetTechnicianByIdAsync(Guid id);
+		Task<bool> ApproveTechnicianAsync(Guid technicianProfileId, string approvedBy);
+		Task<bool> RejectTechnicianAsync(Guid technicianProfileId, string rejectedBy);
+		Task<bool> ApproveTechnicianWithNotificationAsync(Guid technicianProfileId, string approvedBy);
+		Task<bool> RejectTechnicianWithNotificationAsync(Guid technicianProfileId, string rejectedBy);
+
+		#region
+		Task UpdateLocationAsync(UpdateLocationDto input, Guid CurrentTechId);
+		#endregion
+	}
 }
