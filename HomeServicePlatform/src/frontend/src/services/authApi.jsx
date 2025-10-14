bb24bf50d814930cb2af78593b55f1928e604f42
Thanh Long <longnthe172278@fpt.edu.vn>
2025-10-14 23:59:27 +0700

    login with phone number

--- HomeServicePlatform/src/frontend/src/services/authApi.jsx
+++ HomeServicePlatform/src/frontend/src/services/authApi.jsx
@@ -142,10 +142,10 @@ export const authApi = {
     }
   },
 
-  login: async ({ email, password }) => {
+  login: async ({ emailOrPhone, password }) => {
     try {
       const res = await axiosClient.post(`/Authentication/login`, {
-        email,
+        emailOrPhone,
         password,
       });
 
@@ -154,7 +154,7 @@ export const authApi = {
           success: true,
           data: {
             userId: res.data.userId,
-            email: res.data.email,
+            emailOrPhone: res.data.emailOrPhone,
             jwtToken: res.data.jwtToken,
             requirePasswordSetup: res.data.requirePasswordSetup || false,
           },
