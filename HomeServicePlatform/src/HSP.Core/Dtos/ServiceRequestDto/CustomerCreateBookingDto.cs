c134df74fe648d65e5d19ca8c1ca194359501d46
QuanHQ <quanqhhq@gmail.com>
2025-10-17 01:22:32 +0700

    Refactor booking creation to use CustomerProfile

--- HomeServicePlatform/src/HSP.Core/Dtos/ServiceRequestDto/CustomerCreateBookingDto.cs
+++ HomeServicePlatform/src/HSP.Core/Dtos/ServiceRequestDto/CustomerCreateBookingDto.cs
@@ -4,12 +4,9 @@ namespace HSP.Core.Dtos.ServiceRequestDto
 {
 	public class CustomerCreateBookingDto
 	{
-		[Required]
 		public string Address { get; set; }
-		[Required]
 		public List<Guid> ServiceIds { get; set; }
-		[Required]
-		public Guid CustomerId { get; set; }
+		public string CustomerId { get; set; }
 		public double DistanceKm { get; set; } = 50;
 	}
 	public class MatchedBookingResultDto
